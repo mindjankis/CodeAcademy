60d4c879a6cb7232aa602e68b4c72740889fa7c0
mindjankis <mindjankis@yahoo.com>
2023-11-09 10:16:10 +0200

    Merge branch 'main' of https://github.com/mindjankis/CgirodeAcademy

--- devinta_paskaita/bandimas_11.php
+++ devinta_paskaita/bandimas_11.php
@@ -4,7 +4,7 @@ declare (strict_types=1);
 //1. Išspausdinkite šio momento datą pasinaudodami funkcija 'date'
 //
 //function exercise1(): void{
-//    echo(date('Y-m-d'));
+//    echo date('Y-m-d');
 //}
 //echo exercise1();
 
@@ -12,10 +12,9 @@ declare (strict_types=1);
 2. Išspausdinkite datą '2008-12-15 15:15' pasinaudodami funkcija 'date'
 */
 //function exercise2(): void{
-//    $date=new DateTime('2008-12-15 15:15');
-//    print_r($date->format('Y-m-d H:i'));
+//    echo date('2008-12-15 15:15');
 //}
-//echo exercise2();
+//exercise2();
 //
 ///*
 //3. Išspausdinkite šio momento datą skirtingais formatais, kurie atitiktų pavyzdžius:
@@ -51,7 +50,7 @@ declare (strict_types=1);
 //print_r($date->format('Y/m/d h:i:s A'));echo PHP_EOL;
 //$temo='2000 March 2nd 15:30:00';
 //$date =DateTime::createFromFormat('Y F jS H:i:s', $temo);
-//print_r($date->format('Y-F-jS H:i:s'));
+//print_r($date);
 //
 //}
 //echo exercise4();
@@ -75,9 +74,19 @@ declare (strict_types=1);
 //$date2=$date2->modify('+10 years');
 //print_r($date2->format('dS M Y h:i:s A'));echo PHP_EOL;
 //$date3 =new DateTime('15th Jan 2021 8:15:01 PM');
-//$date3=$date3->modify('+5 hours');
+//$date3=$date3->modify('-5 hours');
 //print_r($date3->format('dS M Y h:i:s A'));echo PHP_EOL;
-//echo (date('t'));echo PHP_EOL;
+//$date7 =new DateTime('15th Jan 2021 8:15:01 PM');
+//$date7=$date7->modify('Last day of this month');
+//print_r($date7->format('Y-m-d'));echo PHP_EOL;
+//$date8 =new DateTime('15th Jan 2021 8:15:01 PM');
+//$date8=$date8->modify('First day of this month');
+//print_r($date8->format('Y-m-d'));echo PHP_EOL;
+//$date9 =new DateTime('15th Jan 2021 8:15:01 PM');
+//$date9=$date9->modify('Next Tuesday');
+//print_r($date9->format('Y-m-d'));echo PHP_EOL;
+//$date8 =new DateTime('15th Jan 2021 8:15:01 PM');
+//$date7 =new DateTime('15th Jan 2021 8:15:01 PM');
 //$date4=new DateTime();
 //$date4=($date4->modify('first day of this month'));
 //print_r($date4->format('d'));echo PHP_EOL;
@@ -118,3 +127,209 @@ declare (strict_types=1);
 //    }
 //}
 //exercise6();
+
+//Skripto vykdymo trukmes matavimas
+//$t = 0;
+//$start = hrtime(true);
+//for ($i = 0; $i < 10000000; $i++) {
+//    $t++;
+//}
+//$end = hrtime(true);
+//$duration = $end - $start;
+//
+//echo $duration / 1000000 . PHP_EOL;
+//
+//echo $t;
+
+//function exercise7($date1, $date2): string{
+//    if ($date1>$date2){
+//        $temp='First date is newer';
+//    }
+//    else{$temp='Second date is newer';}
+//    /*
+//    Palyginkite datas ir grąžinkite atsakymą, kuri data naujesnė.
+//
+//    Funkcijos kvietimas: exercise7(date_create('2022-01-25 17:13:25'), date_create('2020-01-25 17:13:25'));
+//    Rezultatas:
+//    'First date is newer'
+//
+//    Funkcijos kvietimas: exercise7(date_create('2020-01-25 17:13:25'), date_create('2022-01-25 17:13:25'));
+//    Rezultatas:
+//    'Second date is newer'
+//    */
+//
+//    return $temp;
+//}
+//echo exercise7(date_create('2022-01-25 17:13:25'), date_create('2020-01-25 17:13:25'));
+
+//function exercise8($date): void{
+//
+//    /*
+//    Išspausdinkite paduotos datos skirtumą nuo dabartinio momento žodžiais.
+//
+//    Funkcijos kvietimas: exercise8(date_create('2020-01-25 17:13:25'));
+//    Rezultatas:
+//    Supplied date was 891 days ago
+//
+//    Funkcijos kvietimas: exercise8(date_create('2024-01-25 17:13:25'));
+//    Rezultatas:
+//    Supplied date is in the future
+//    */
+//    $datenow=new DateTime();
+//    if($date>$datenow){
+//        echo 'Supplied date is in the future';
+//    }
+//    else {
+//        $interval = $date->diff($datenow);
+//        print_r($interval->format('%a days'));echo ' ago';
+//    }
+//
+//}
+//exercise8(date_create('2020-01-25 17:13:25'));echo PHP_EOL;
+//exercise8(date_create('2024-01-25 17:13:25'));
+//
+//function exercise9($date): void
+//{
+//    /*
+//    Išspausdinkite datų skirtumą žodžiais.
+//
+//    Funkcijos kvietimas: exercise9(date_create('2020-01-25 17:13:25'));
+//    Rezultatas:
+//    Supplied date was 2 years 1 months 11 days
+//
+//    Funkcijos kvietimas: exercise9(date_create('2024-01-25 17:13:25'));
+//    Rezultatas:
+//    Supplied date is in the future
+//    */
+//    $datenow=new DateTime();
+//    if($date>$datenow){
+//        echo 'Supplied date is in the future';
+//    }
+//    else {
+//        $interval = $date->diff($datenow);
+//        print_r($interval->format('%y years %m months %d days'));
+//    }
+//
+//
+//}
+//exercise9(date_create('2020-01-25 17:13:25'));echo PHP_EOL;
+//exercise9(date_create('2024-01-25 17:13:25'));
+
+//function exercise10(): array
+//{
+//    $products = [
+//        [
+//            'name' => 'Wine glass',
+//            'last_purchase' => '2023 Jan 15 18:34:12',
+//        ],
+//        [
+//            'name' => 'Bread knife',
+//            'last_purchase' => '2020 Mar 15 23:14:00',
+//        ],
+//        [
+//            'name' => 'Blue chair',
+//            'last_purchase' => '2019 Dec 12 15:00:12',
+//        ],
+//        [
+//            'name' => 'Cutting board',
+//            'last_purchase' => '2023 Feb 1 03:15:01',
+//        ],
+//    ];
+//
+//    /*
+//    Grąžinkite iš funkcijos masyvą tik su tais produktais, kurie paskutinį kartą buvo pirkti einamaisiais metais.
+//    Ši funkcija turėtų veikti ir bet kuriais ateinančiais metais (2023, 2024 ir t.t.)
+//    */
+//    $result=[];
+//    $datenow=new DateTime();
+//    $datenowyear=$datenow->format('Y');
+//    $datenowyear=intval($datenowyear);
+//foreach ($products as $product){
+//    $temp=$product['last_purchase'];
+//    $date= DateTime::createFromFormat('Y M d H:i:s', $temp);
+//    $dateyear=$date->format('Y');
+//    $dateyear=intval($dateyear);
+//    if($datenowyear==$dateyear){
+//        $result[]=$product;
+//    }
+//}
+//    return $result;
+//}
+//print_r(exercise10());
+
+//function exercise11(bool $showOnlyDays): void
+//{
+//    $products = [
+//        [
+//            'name' => 'Wine glass',
+//            'last_purchase' => '2022 Jan 15 18:34:12',
+//        ],
+//        [
+//            'name' => 'Bread knife',
+//            'last_purchase' => '2020 Mar 15 23:14:00',
+//        ],
+//        [
+//            'name' => 'Blue chair',
+//            'last_purchase' => '2019 Dec 12 15:00:12',
+//        ],
+//        [
+//            'name' => 'Cutting board',
+//            'last_purchase' => '2022 Feb 1 03:15:01',
+//        ],
+//    ];
+//
+//    /*
+//    Išspausdinkite paskutinių pirkimų santrauką. Jeigu $showOnlyDays yra true, tuomet rodykite tik dienas.
+//
+//    Funkcijos kvietimas: exercise11(false)
+//    Rezultatas:
+//    Last purchased:
+//    Wine glass 0 years 1 month 23 days ago
+//    ...
+//
+//    Funkcijos kvietimas: exercise11(true)
+//    Rezultatas:
+//    Last purchased:
+//    Wine glass 51 days ago
+//    ...
+//    */
+//    $datenow=new DateTime();
+//    foreach ($products as $product) {
+//        $temp = $product['last_purchase'];
+//        $date = DateTime::createFromFormat('Y M d H:i:s', $temp);
+//        if($showOnlyDays){
+//            $interval=$datenow->diff($date);
+//            print_r($product['name']); echo ' ';
+//            print_r($interval->format('%a days ago'));echo PHP_EOL;
+//        }
+//            else
+//            $interval=$datenow->diff($date);
+//            {print_r($product['name']); echo ' ';
+//            print_r($interval->format('%Y years %m month %d days ago')); echo PHP_EOL;
+//            }
+//
+//
+//
+//    }
+//}
+//    exercise11(false);
+//
+//function exercise12(int $numberOfCycles): void
+//{
+//    /*
+//    Išspausdinkite kiek laiko trunka prasukti tuščią ciklą nurodytą kiekį kartų ($numberOfCycles).
+//    Trukmę apvalinkite iki milisekundžių.
+//    Pridėkite parametrui $numberOfCycles numatytąją reikšmę 1000000.
+//    */
+//    $t = 0;
+//$start = hrtime(true);
+//for ($i = 0; $i < $numberOfCycles; $i++) {
+//    $t++;
+//}
+//$end = hrtime(true);
+//$duration = $end - $start;
+//
+//echo $duration / 1000000 . PHP_EOL;
+//echo $t;
+//}
+//exercise12(1000000);
