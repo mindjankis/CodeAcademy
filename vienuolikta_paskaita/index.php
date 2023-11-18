56ec6ee2cd95af04ea9e523deede4c54d8208a6d
mindjankis <mindjankis@yahoo.com>
2023-11-18 12:03:30 +0200

    Merge branch 'main' of https://github.com/mindjankis/CgirodeAcademy

--- vienuolikta_paskaita/index.php
+++ vienuolikta_paskaita/index.php
@@ -1,5 +1,4 @@
-<?php
-declare (strict_types=1);
+<?php declare (strict_types=1);
 if(file_exists('todo_database.json')){
     $todosarray=json_decode(file_get_contents('todo_database.json'),true);
     }
@@ -12,37 +11,53 @@ else{
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Title</title>
+  <meta charset="UTF-8">
+  <title>Title</title>
 </head>
 <body>
-            <form method="POST" action="submit.php">
-                New TODO:<input name="todo" type="text">
-                Due date:<input name="date" type="date" >
-                Due time:<input name="time" type="time" >
-            <button type="submit"> Submit</button>
-            </form>
+<table border="2">
+  <tbody>
+    <tr>
+      <td>
+      <form style="height: 39px; width: 700px; background-color: #5e8949" method="post" action="submit.php">
+<span style="font-weight: bold;">New TODO:</span><input name="todo" type="text"> 
+<span style="font-weight: bold;">Due date:</span><input name="date" type="date"> 
+<span style="font-weight: bold;">Due time:</span><input name="time" type="time">
+<button type="submit">Submit</button>
+      </form>
+      </td>
+    </tr>
+  </tbody>
+</table>
 <br>
 <table border="2">
+  <tbody>
     <tr>
-        <th style="width: 300px;">TODOs</th>
-        <th style="width: 300px;">Created at</th>
-        <th style="width: 300px;">Due date</th>
+      <th style="width: 300px; background-color: aqua">TODOs</th>
+      <th style="width: 300px; background-color: aqua">Created at</th>
+      <th style="width: 300px; background-color: aqua">Due date</th>
     </tr>
+  </tbody>
 </table>
+
 <?php foreach ($todosarray as $key=>$value):?>
 <table border="2">
+  <tbody>
     <tr>
-        <td style="width: 300px;"><?php echo $value['todo'];?></td>
-        <td style="width: 300px;"><?php echo $value['curent_date'];?></td>
-        <td style="width: 300px;"><?php echo $value['date'].' '.$value['time']?></td>
-        <td>
-            <form action='delete.php' method='post'>
-                <button type='submit' name='deleteBtn' value="<?php echo $key;?>">Delete</button>
-            </form>
-        </td>
-        <?php endforeach;?>
+      <td style="width: 300px;"><?php echo $value['todo'];?><br>
+      </td>
+      <td style="width: 300px;"><?php echo $value['curent_date'];?><br>
+      </td>
+      <td style="width: 300px;"><?php echo $value['date'].' '.$value['time']?><br>
+      </td>
+      <td>
+      <form action="delete.php" method="post"> 
+<button type="submit" name="deleteBtn" value="<?php echo $key;?>">Delete</button>
+</form>
+      </td>
+<?php endforeach;?>
     </tr>
+</tbody>
 </table>
 </body>
 </html>
