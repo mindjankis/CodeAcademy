7f1611ece5eca7e7fb9f4250457c1bdeddff306d
mindjankis <mindjankis@yahoo.com>
2023-11-21 14:32:41 +0200

    Merge branch 'main' of https://github.com/mindjankis/CgirodeAcademy

--- vienuolikta_paskaita/submit.php
+++ vienuolikta_paskaita/submit.php
@@ -1,11 +1,18 @@
 <?php
 declare (strict_types=1);
     $todosarray=json_decode(file_get_contents('todo_database.json'),true);
-    $todolist=$_POST;
+    //var_dump($_POST);
     $curentDate=new DateTime();
     $curentDate=$curentDate->format('Y-m-d H:i');
-    $todolist['curent_date']=$curentDate;
-    $todosarray[]=$todolist;
-    file_put_contents('todo_database.json',json_encode($todosarray),JSON_PRETTY_PRINT);
+    $newarray=[];
+    $newarray=[
+        'todo'=>trim($_POST['todo']),
+        'date'=>trim($_POST['date']),
+        'time'=>trim($_POST['time']),
+        'curent_date'=>$curentDate
+    ];
+    var_dump($newarray);
+    $todosarray[]=$newarray;
+    file_put_contents('todo_database.json',json_encode($todosarray,JSON_PRETTY_PRINT));
 header('Location: index.php');
 
